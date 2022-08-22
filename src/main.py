ed697240eed3b6c9ab4e47d893f71a8b55acac88
eldos <eldos.baktybekov@gmail.com>
2022-08-22 16:10:46 +0600

    :construction: Refactor entrypoint

--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
 import uvicorn
 
-from app import app
-from settings import get_app_settings, init_settings
+from fastapi import FastAPI
+
+import api
+from settings import get_app_settings
+
+
+def get_application() -> FastAPI:
+    app_settings = get_app_settings()
+    application = FastAPI(
+        debug=app_settings.debug,
+        host=app_settings.host,
+        port=app_settings.port,
+        reload=app_settings.debug,
+    )
+    application.include_router(api.events.router)
+    return application
 
 
 def main():
-    init_settings()
-    app_settings = get_app_settings()
-    uvicorn.run(
-        'app:app',
-        host=app_settings.host,
-        port=app_settings.port,
-        debug=app_settings.debug,
-        reload=app_settings.debug,
-    )
+    uvicorn.run('main:app')
 
+
+app = get_application()
 
 if __name__ == '__main__':
     main()
