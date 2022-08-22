afdeb5efbc217e8747869d3c83e788b2675dceb5
eldos <eldos.baktybekov@gmail.com>
2022-08-22 14:24:25 +0600

    :sparkles: Add ping message generator

---
+++ src/resources/events.py
@@ -0,0 +1,16 @@
+import datetime
+
+from sse_starlette import ServerSentEvent
+
+__all__ = (
+    'get_ping_message',
+)
+
+
+def get_ping_message() -> ServerSentEvent:
+    return ServerSentEvent(
+        event='ping',
+        data={
+            'time': datetime.datetime.utcnow(),
+        },
+    )
