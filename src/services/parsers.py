5398cee92769a67c652670bb28b301137796fb43
eldos <eldos.baktybekov@gmail.com>
2022-08-24 18:02:33 +0600

    :recycle: Refactor function parameters

--- src/services/parsers.py
+++ src/services/parsers.py
@@ -2,7 +2,11 @@ import models
 from resources import time_helpers
 
 
-def is_passed_periodic_time(passed_seconds: int, interval_in_seconds: int = 600, deviation: int = 20) -> bool:
+def is_passed_periodic_time(
+        passed_seconds: int | float,
+        interval_in_seconds: int,
+        deviation_in_seconds: int,
+) -> bool:
     """Determine if precise time (or with deviation) has been passed since 0 seconds.
     It starts counting from 0 to N with some interval.
 
@@ -24,36 +28,38 @@ def is_passed_periodic_time(passed_seconds: int, interval_in_seconds: int = 600,
     True will be returned.
 
     Examples:
-        >>> is_passed_periodic_time(passed_seconds=60, interval_in_seconds=60, deviation=0)
+        >>> is_passed_periodic_time(passed_seconds=60, interval_in_seconds=60, deviation_in_seconds=0)
         True
-        >>> is_passed_periodic_time(passed_seconds=33, interval_in_seconds=10, deviation=2)
+        >>> is_passed_periodic_time(passed_seconds=33, interval_in_seconds=10, deviation_in_seconds=2)
         False
-        >>> is_passed_periodic_time(passed_seconds=31, interval_in_seconds=10, deviation=2)
+        >>> is_passed_periodic_time(passed_seconds=31, interval_in_seconds=10, deviation_in_seconds=2)
         True
 
     Args:
         passed_seconds: Passed seconds since 0.
         interval_in_seconds: Values between breakpoints.
-        deviation: Deviation from the breakpoints
+        deviation_in_seconds: Deviation from the breakpoints
 
     Returns:
         True if range (calculated with intervals and deviations) includes passed seconds, otherwise False.
     """
     if passed_seconds < 0:
         raise ValueError('Passed time can not be negative')
-    if interval_in_seconds <= deviation:
+    if interval_in_seconds <= deviation_in_seconds:
         raise ValueError('Deviation can not be bigger than interval')
 
     interval_multiplier = round(passed_seconds / interval_in_seconds)
     comparable_threshold = interval_multiplier * interval_in_seconds
-    return (comparable_threshold - deviation) <= passed_seconds <= (comparable_threshold + deviation)
+    from_seconds_threshold = comparable_threshold - deviation_in_seconds
+    to_seconds_threshold = comparable_threshold + deviation_in_seconds
+    return from_seconds_threshold <= passed_seconds <= to_seconds_threshold
 
 
 def determine_event_type_or_none(write_off: models.NotWrittenOffIngredient) -> models.EventType | None:
     seconds_before_expire = (write_off.write_off_at - time_helpers.get_moscow_datetime_now()).total_seconds()
     if seconds_before_expire <= 0 and is_passed_periodic_time(passed_seconds=abs(seconds_before_expire),
                                                               interval_in_seconds=600,
-                                                              deviation=20):
+                                                              deviation_in_seconds=20):
         return models.EventType.ALREADY_EXPIRED
     event_types_by_ranges = (
         (880, 920, models.EventType.EXPIRE_AT_15_MINUTES),
