d4f3117b493af488d4ec72de54d5ca6591644642
Eldos <eldos.baktybekov@gmail.com>
2023-02-18 12:35:16 +0600

    :wrench: GI-19 Change configuration way

--- src/settings.py
+++ src/settings.py
@@ -1,31 +1,20 @@
-from functools import lru_cache
-
 from dotenv import load_dotenv
 from pydantic import BaseSettings, Field
 
 __all__ = (
-    'get_app_settings',
-    'get_rabbitmq_settings',
+    'Settings',
+    'settings',
 )
 
 load_dotenv()
 
 
-class AppSettings(BaseSettings):
+class Settings(BaseSettings):
     host: str = Field(env='APP_HOST')
     port: int = Field(env='APP_PORT')
     debug: bool = Field(env='DEBUG')
     token: str = Field(env='TOKEN')
+    rabbitmq_url: str = Field(env='RABBITMQ_URL')
 
 
-class RabbitMQSettings(BaseSettings):
-    url: str = Field(env='RABBITMQ_URL')
-
-
-@lru_cache
-def get_app_settings() -> AppSettings:
-    return AppSettings()
-
-
-def get_rabbitmq_settings() -> RabbitMQSettings:
-    return RabbitMQSettings()
+settings = Settings()
