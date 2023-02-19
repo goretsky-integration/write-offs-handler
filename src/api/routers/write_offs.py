ec5cff863cb424b60f2ad08655e10fad5bb0993f
Eldos <eldos.baktybekov@gmail.com>
2023-02-19 11:00:19 +0600

    :sparkles: GI-19 Add write-offs creation endpoint

--- src/api/routers/write_offs.py
+++ src/api/routers/write_offs.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Response, status, Depends
 
-from api import schemas
+from api import schemas, dependencies
+from repositories import WriteOffRepository
+from services.external_database import ExternalDatabaseService
 
 router = APIRouter()
 
@@ -11,7 +13,15 @@ router = APIRouter()
 )
 def create_write_off(
         write_off: schemas.WriteOffCreate,
+        write_offs: WriteOffRepository = Depends(dependencies.get_write_offs_repository),
+        external_database_service: ExternalDatabaseService = Depends(dependencies.get_external_database_service),
 ):
+    unit = external_database_service.get_units(name=write_off.unit_name)
+    write_offs.create(
+        unit_id=unit.id,
+        ingredient_name=write_off.ingredient_name,
+        to_be_written_off_at=write_off.to_be_written_off_at,
+    )
     return Response(status_code=status.HTTP_201_CREATED)
 
 
