ee1465a9f2cd6bfb371198b14bc3f7e26e62aed0
Eldos <eldos.baktybekov@gmail.com>
2023-02-19 10:58:07 +0600

    :construction: GI-19 Add repository and database service dependencies

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,7 +1,17 @@
 from fastapi import Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.orm import sessionmaker
 
-__all__ = ('TokenBearer',)
+from database.engine import engine
+from repositories import WriteOffRepository
+from services.external_database import ExternalDatabaseService
+from settings import settings
+
+__all__ = (
+    'TokenBearer',
+    'get_write_offs_repository',
+    'get_external_database_service',
+)
 
 
 class TokenBearer(HTTPBearer):
@@ -12,3 +22,11 @@ class TokenBearer(HTTPBearer):
     async def __call__(self, request: Request):
         credentials: HTTPAuthorizationCredentials = await super().__call__(request)
         return credentials.credentials
+
+
+def get_write_offs_repository() -> WriteOffRepository:
+    return WriteOffRepository(sessionmaker(engine))
+
+
+def get_external_database_service() -> ExternalDatabaseService:
+    return ExternalDatabaseService(base_url=settings.external_database_api_url)
