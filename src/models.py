77a7caa02e33674b29a955fa314b7795bfe7f7ae
Eldos <eldos.baktybekov@gmail.com>
2023-02-19 10:55:54 +0600

    :construction: GI-19 Add database api response models

---
+++ src/models.py
@@ -0,0 +1,13 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+__all__ = ('Unit',)
+
+
+class Unit(BaseModel):
+    id: int
+    name: str
+    uuid: UUID
+    account_name: str
+    region: str
