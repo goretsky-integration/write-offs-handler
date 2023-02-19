abe8b24179e8682fb40c6a3dcd5db9170306bdc0
Eldos <eldos.baktybekov@gmail.com>
2023-02-19 10:56:40 +0600

    :sparkles: GI-19 Add external database service

---
+++ src/services/external_database.py
@@ -0,0 +1,19 @@
+import httpx
+
+import exceptions
+import models
+
+__all__ = ('ExternalDatabaseService',)
+
+
+class ExternalDatabaseService:
+
+    def __init__(self, *, base_url: str):
+        self.__base_url = base_url
+
+    def get_units(self, *, name: str) -> models.Unit:
+        url = f'{self.__base_url}/units/name/{name}/'
+        response = httpx.get(url)
+        if response.status_code == 404:
+            raise exceptions.UnitNotFound
+        return models.Unit.parse_obj(response.json())
