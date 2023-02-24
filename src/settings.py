624d69616b88c9ad7b2486efab2f6bcd315b59e9
Eldos <eldos.baktybekov@gmail.com>
2023-02-24 22:42:24 +0600

    :loud_sound: Add logging in write-offs endpoint

--- src/settings.py
+++ src/settings.py
@@ -6,6 +6,7 @@ from pydantic import BaseSettings, Field
 __all__ = (
     'get_app_settings',
     'get_rabbitmq_settings',
+    'logging_settings',
 )
 
 load_dotenv()
@@ -16,6 +17,11 @@ class AppSettings(BaseSettings):
     port: int = Field(env='APP_PORT')
     debug: bool = Field(env='DEBUG')
     token: str = Field(env='TOKEN')
+
+
+class LoggingSettings(BaseSettings):
+    level: str = Field(env='LOGLEVEL')
+    logfile_path: str | None = Field(default=None, env='LOGFILE_PATH')
 
 
 class RabbitMQSettings(BaseSettings):
@@ -29,3 +35,6 @@ def get_app_settings() -> AppSettings:
 
 def get_rabbitmq_settings() -> RabbitMQSettings:
     return RabbitMQSettings()
+
+
+logging_settings = LoggingSettings()
